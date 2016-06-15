1f390e5c7c4012fe9cea6402851a657ee8cd374a
Alistair Stead <alistair_stead@me.com>
2016-06-15 11:32:46 +0100

    Corrected the path to the hooks

--- git/templates/hooks/run-hooks.sh
+++ git/templates/hooks/run-hooks.sh
@@ -4,7 +4,7 @@ EXIT_CODE=0
 
 repo=$( git rev-parse --show-toplevel )
 hook_type=$( basename $0 )
-hooks=~/src/dotfiles/git/hooks
+hooks=~/dotfiles/git/hooks
 
 echo "Executing $hook_type hook(s)"
 
