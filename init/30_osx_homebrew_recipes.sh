73792426d3dd9e00bb3ac8639f9484fc4b2c0dfc
Alistair Stead <ailstair_stead@me.com>
2016-07-17 01:16:42 +0200

    Add recipes and taps

--- init/30_osx_homebrew_recipes.sh
+++ init/30_osx_homebrew_recipes.sh
@@ -4,52 +4,59 @@ is_osx || return 1
 # Exit if Homebrew is not installed.
 [[ ! "$(type -P brew)" ]] && e_error "Brew recipes need Homebrew to install." && return 1
 
+kegs=(homebrew/versions)
+brew_tap_kegs
+
 # Homebrew recipes
 recipes=(
-  android-platform-tools
-  ansible
   bash
-  cmatrix
-  cowsay
+  bash-completion2
+  grc
+  gpg
+  keybase
+  'wget --enable-iri'
+  #vim --override-system-vi
+  neovim/neovim/neovim
+  gh
+  hub
+  #mtr - ping & traceroute. best.
+  mtr
+  ack
   git
   git-extras
-  htop-osx
-  hub
-  id3tool
-  lesspipe
-  man2html
-  mercurial
-  nmap
-  postgres
-  sl
-  ssh-copy-id
-  terminal-notifier
-  the_silver_searcher
   tree
-  wget
+  ssh-copy-id
+  zsh
+  heroku-toolbelt
+  trash
+  python3
+  'findutils --default-names'
+  'gnu-sed --default-names'
+  'gnu-tar --default-names'
+  'gnu-which --default-names'
+  'gnutls --default-names'
+  'grep --default-names'
+  coreutils
+  moreutils
+  binutils
+  diffutils
+  gzip
+  watch
+  tmux
+  nmap
+  htop
+  selenium-server-standalone
+  chromedriver
+  jmeter
+  mackup
+  homebrew/binary/ngrok2
+  reattach-to-user-namespace
 )
 
 brew_install_recipes
 
-# Misc cleanup!
-
-# This is where brew stores its binary symlinks
-local binroot="$(brew --config | awk '/HOMEBREW_PREFIX/ {print $2}')"/bin
-
-# htop
-if [[ "$(type -P $binroot/htop)" ]] && [[ "$(stat -L -f "%Su:%Sg" "$binroot/htop")" != "root:wheel" || ! "$(($(stat -L -f "%DMp" "$binroot/htop") & 4))" ]]; then
-  e_header "Updating htop permissions"
-  sudo chown root:wheel "$binroot/htop"
-  sudo chmod u+s "$binroot/htop"
-fi
-
-# bash
-if [[ "$(type -P $binroot/bash)" && "$(cat /etc/shells | grep -q "$binroot/bash")" ]]; then
-  e_header "Adding $binroot/bash to the list of acceptable shells"
-  echo "$binroot/bash" | sudo tee -a /etc/shells >/dev/null
-fi
-if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/bash" ]]; then
-  e_header "Making $binroot/bash your default shell"
-  sudo chsh -s "$binroot/bash" "$USER" >/dev/null 2>&1
+if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/zsh" ]]; then
+  e_header "Making $binroot/zsh your default shell"
+  sudo chsh -s "$binroot/zsh" "$USER" >/dev/null 2>&1
   e_arrow "Please exit and restart all your shells."
 fi
