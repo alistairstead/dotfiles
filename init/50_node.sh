6c3f9bd2a4464770438d045e7343b2bd2a63712d
Alistair Stead <ailstair_stead@me.com>
2016-07-26 18:24:21 +0100

    Refactored the nvn and node install

--- init/50_node.sh
+++ init/50_node.sh
@@ -1,3 +1,35 @@
-nvm install stable
+#!/usr/bin/env bash
 
-npm install -g diff-so-fancy tern elm elm-test elm-oracle
+# because I use a lazy loading strategy for nvm I need to source this file
+# or the command check will fail witin the execution of install_dotfiles
+source $DOTFILES/rc.d/functions.sh
+
+# Confirm if a command exists
+function _nvm_exists () {
+    \command -v ${1} > /dev/null 2>&1 || {
+      return 1;
+    }
+}
+
+function install_nvm() {
+  if ! _nvm_exists 'nvm'; then
+    e_header 'Installing nvm...'
+    curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+    e_header 'Reloading the shell'
+    exec $SHELL -l && echo 'nvm installed!'
+  else
+    e_header 'nvm is already installed'
+  fi
+}
+
+function install_node_stable() {
+  nvm install stable
+}
+
+function install_node_global_packages() {
+  npm install -g diff-so-fancy tern elm elm-test elm-oracle
+}
+
+install_nvm && install_node_stable && install_node_global_packages
+
+
