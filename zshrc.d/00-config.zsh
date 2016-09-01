a19504cae82f5020fa2e2ad7ea8e42503e956ac1
Alistair Stead <ailstair_stead@me.com>
2016-09-01 15:50:02 +0100

    Moved config to prezto

--- zshrc.d/00-config.zsh
+++ zshrc.d/00-config.zsh
@@ -1,84 +1,16 @@
-#if [[ -n $SSH_CONNECTION ]]; then
-#  export PS1='%m:%3~$(git_info_for_prompt)%# '
-#else
-#  export PS1='%3~$(git_info_for_prompt)%# '
-#fi
-
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
-setopt nobeep                  # i hate beeps
-setopt autopushd               # automatically append dirs to the push/pop list
-setopt cdablevars              # avoid the need for an explicit $
-setopt correct_all             # correct all the words in the command line
-setopt noflowcontrol           # if I could disable Ctrl-s completely I would!
 setopt RM_STAR_WAIT            # confirmation after 'rm *' etc..
 
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
-
-setopt PROMPT_SUBST
-setopt CORRECT
-setopt IGNORE_EOF
-
 bindkey '^?' backward-delete-char
-bindkey "^P" history-substring-search-up
-bindkey "^N" history-substring-search-down
-bindkey "^R" history-incremental-search-backward
 bindkey "^L" forward-word
 bindkey "^H" backward-word
 bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
 
-# bind UP and DOWN arrow keys for history search
-zmodload zsh/terminfo
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
-
-
-setopt autocd                   # change to dirs without cd
-setopt pushd_to_home            # Push to home directory when no argument is given.
-setopt auto_pushd               # Push the old directory onto the stack on cd.
-setopt auto_name_dirs           # Auto add variable-stored paths to ~ list.
-setopt pushd_ignore_dups        # Do not store duplicates in the stack.
 
 # Report CPU usage for commands running longer than 10 seconds.
 export TIMEFMT="%U user %S system %P cpu %*E total, running %J"
 REPORTTIME=10
 
-#ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
-
-## Aliases and functions
-#ZSH_HIGHLIGHT_STYLES[alias]='fg=blue,bold'
-#ZSH_HIGHLIGHT_STYLES[function]='fg=cyan,bold'
-
-## Commands and builtins
-#ZSH_HIGHLIGHT_STYLES[command]="fg=green"
-#ZSH_HIGHLIGHT_STYLES[hashed-command]="fg=green,bold"
-#ZSH_HIGHLIGHT_STYLES[builtin]="fg=green,bold"
-#ZSH_HIGHLIGHT_STYLES[precommand]="fg=green,underline"
-#ZSH_HIGHLIGHT_STYLES[commandseparator]="none"
-
-## Paths
-#ZSH_HIGHLIGHT_STYLES[path]='fg=white,underline'
-
-## Globbing
-#ZSH_HIGHLIGHT_STYLES[globbing]='fg=yellow,bold'
-
-## Options and arguments
-#ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=red'
-#ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=red'
-
-#ZSH_HIGHLIGHT_STYLES[back-quoted-argument]="fg=green"
-#ZSH_HIGHLIGHT_STYLES[single-quoted-argument]="fg=green"
-#ZSH_HIGHLIGHT_STYLES[double-quoted-argument]="fg=green"
-#ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]="fg=green"
-#ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]="fg=green"
-
-## Patterns
-#ZSH_HIGHLIGHT_PATTERNS+=('mv *' 'fg=white,bold,bg=red')
-#ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
-#ZSH_HIGHLIGHT_PATTERNS+=('sudo ' 'fg=white,bold,bg=red')
--- zshrc.d/10-zplug.zsh
+++ zshrc.d/10-zplug.zsh
@@ -1,4 +1,4 @@
-# Check if zplug is installed
+#Check if zplug is installed
 if [[ ! -f "$HOME/.zplug/init.zsh" ]]; then
   git clone https://github.com/zplug/zplug "$HOME/.zplug/repos/zplug/zplug"
   ln -s "$HOME/.zplug/repos/zplug/zplug/init.zsh" "$HOME/.zplug/init.zsh"
@@ -8,36 +8,16 @@ fi
 source $HOME/.zplug/init.zsh
 
 zplug "zplug/zplug"
+zplug "marzocchi/zsh-notify"
+zplug "tcnksm/docker-alias", \
+      use:zshrc
 
-zplug "plugins/brew", from:oh-my-zsh
-zplug "plugins/brew-cask", from:oh-my-zsh
-zplug "plugins/command-not-found", from:oh-my-zsh
-zplug "plugins/composer", from:oh-my-zsh
-zplug "plugins/docker", from:oh-my-zsh
-zplug "plugins/extract", from:oh-my-zsh
-zplug "plugins/heroku", from:oh-my-zsh
-zplug "plugins/httpie", from:oh-my-zsh
-zplug "plugins/last-working-dir", from:oh-my-zsh
-zplug "plugins/lol", from:oh-my-zsh
-zplug "plugins/sudo", from:oh-my-zsh
-zplug "plugins/symfony2", from:oh-my-zsh
+#zplug "sorin-ionescu/prezto", use:"*.zsh"
 
-#zplug "lib/clipboard", from:oh-my-zsh, if:"[[ $OSTYPE == *darwin* ]]"
+zplug "knu/z", \
+      use:z.sh, \
+      nice:10
 
-zplug "tcnksm/docker-alias", use:zshrc
-
-#zplug "zsh-users/zsh-completions"
-zplug "zsh-users/zsh-autosuggestions"
-
-zplug "knu/z", use:z.sh, nice:10
-zplug "zsh-users/zsh-syntax-highlighting", nice:10
-zplug "trapd00r/zsh-syntax-highlighting-filetypes", nice:10
-
-zplug "zsh-users/zsh-history-substring-search"
-
-##zplug "arialdomartini/oh-my-git"
-
-zplug "sindresorhus/pure"
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
@@ -51,3 +31,43 @@ fi
 
 # Then, source plugins and add commands to $PATH
 zplug load
+
+if zplug check zsh-users/zsh-autosuggestions; then
+  bindkey '^ ' autosuggest-accept
+fi
+
+if zplug check zsh-users/zsh-syntax-highlighting; then
+  ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+
+  # Aliases and functions
+  ZSH_HIGHLIGHT_STYLES[alias]='fg=blue,bold'
+  ZSH_HIGHLIGHT_STYLES[function]='fg=cyan,bold'
+
+  # Commands and builtins
+  ZSH_HIGHLIGHT_STYLES[command]="fg=green"
+  ZSH_HIGHLIGHT_STYLES[hashed-command]="fg=green,bold"
+  ZSH_HIGHLIGHT_STYLES[builtin]="fg=green,bold"
+  ZSH_HIGHLIGHT_STYLES[precommand]="fg=green,underline"
+  ZSH_HIGHLIGHT_STYLES[commandseparator]="none"
+
+  # Paths
+  ZSH_HIGHLIGHT_STYLES[path]='fg=white,underline'
+
+  # Globbing
+  ZSH_HIGHLIGHT_STYLES[globbing]='fg=yellow,bold'
+
+  # Options and arguments
+  ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=red'
+  ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=red'
+
+  ZSH_HIGHLIGHT_STYLES[back-quoted-argument]="fg=green"
+  ZSH_HIGHLIGHT_STYLES[single-quoted-argument]="fg=green"
+  ZSH_HIGHLIGHT_STYLES[double-quoted-argument]="fg=green"
+  ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]="fg=green"
+  ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]="fg=green"
+
+  # Patterns
+  ZSH_HIGHLIGHT_PATTERNS+=('mv *' 'fg=white,bold,bg=red')
+  ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
+  ZSH_HIGHLIGHT_PATTERNS+=('sudo ' 'fg=white,bold,bg=red')#statements
+fi
