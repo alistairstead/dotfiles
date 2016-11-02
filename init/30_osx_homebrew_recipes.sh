9f2261e8c31bc2152747b150b88e1e31e7fe2fba
Alistair Stead <alistairstead@users.noreply.github.com>
2016-11-02 21:15:15 +0000

    Correct the shell configuration

--- init/30_osx_homebrew_recipes.sh
+++ init/30_osx_homebrew_recipes.sh
@@ -61,6 +61,16 @@ IFS=$'\n'
 brew_install_recipes
 unset IFS
 
+# Misc cleanup!
+
+# This is where brew stores its binary symlinks
+local binroot="$(brew --config | awk '/HOMEBREW_PREFIX/ {print $2}')"/bin
+
+# bash
+if [[ "$(type -P $binroot/zsh)" && "$(cat /etc/shells | grep -q "$binroot/zsh")" ]]; then
+  e_header "Adding $binroot/zsh to the list of acceptable shells"
+  echo "$binroot/zsh" | sudo tee -a /etc/shells >/dev/null
+fi
 if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/zsh" ]]; then
   e_header "Making $binroot/zsh your default shell"
   sudo chsh -s "$binroot/zsh" "$USER" >/dev/null 2>&1
