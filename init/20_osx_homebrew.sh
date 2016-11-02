a68701ec9b7ce4b6cb322da43ed5173560ad843f
Alistair Stead <alistairstead@users.noreply.github.com>
2016-11-02 16:26:55 +0000

    Correct typo

--- init/20_osx_homebrew.sh
+++ init/20_osx_homebrew.sh
@@ -33,7 +33,7 @@ function brew_install_recipes() {
   if (( ${#recipes[@]} > 0 )); then
     e_header "Installing Homebrew recipes: ${recipes[*]}"
     for recipe in "${recipes[@]}"; do
-      e_header "Install ${recipes[@]"
+      e_header "Install ${recipes[@]}"
       brew install $recipe
     done
   fi
