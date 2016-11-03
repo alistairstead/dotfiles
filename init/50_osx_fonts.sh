600367e2a67ed421c281e9b791dd63b54badda6f
Alistair Stead <alistairstead@users.noreply.github.com>
2016-11-03 10:34:52 +0000

    Add fonts using brew

--- init/50_osx_fonts.sh
+++ init/50_osx_fonts.sh
@@ -1,17 +1,44 @@
 # OSX-only stuff. Abort if not OSX.
 is_osx || return 1
 
-# Copy fonts
-{
-  pushd $DOTFILES/conf/osx/fonts/; setdiffA=(*); popd
-  pushd ~/Library/Fonts/; setdiffB=(*); popd
-  setdiff
-} >/dev/null
+# Exit if Homebrew is not installed.
+[[ ! "$(type -P brew)" ]] && e_error "Brew recipes need Homebrew to install." && return 1
 
-if (( ${#setdiffC[@]} > 0 )); then
-  e_header "Copying fonts (${#setdiffC[@]})"
-  for f in "${setdiffC[@]}"; do
-    e_arrow "$f"
-    cp "$DOTFILES/conf/osx/fonts/$f" ~/Library/Fonts/
-  done
-fi
+# Tap Homebrew kegs.
+function brew_tap_kegs() {
+  if (( ${#kegs[@]} > 0 )); then
+    e_header "Tapping Homebrew kegs: ${kegs[*]}"
+    IFS=$'\n'
+    for keg in "${kegs[@]}"; do
+      brew tap $keg
+    done
+    unset IFS
+  fi
+}
+
+# Install Homebrew recipes.
+function brew_install_fonts() {
+  if (( ${#fonts[@]} > 0 )); then
+    e_header "Installing Homebrew fonts: ${fonts[*]}"
+    IFS=$'\n'
+    for font in "${fonts[@]}"; do
+      e_header "Installing font: ${font}"
+      brew cask install $font
+    done
+    unset IFS
+  fi
+}
+
+
+kegs=(
+  caskroom/fonts
+)
+
+brew_tap_kegs
+
+# Homebrew recipes
+fonts=(
+  font-awesome-terminal-fonts
+)
+
+brew_install_fonts
