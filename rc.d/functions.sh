9363312a19fe0c6fe0a8b7623c022766647cd543
Alistair Stead <alistairstead@users.noreply.github.com>
2016-11-03 14:54:44 +0000

    Simplify the node calls and correct path

--- rc.d/functions.sh
+++ rc.d/functions.sh
@@ -244,22 +244,19 @@ vim() {
 
 nvm() {
     unset -f nvm
-    export NVM_DIR=~/.nvm
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
     nvm "$@"
 }
 
 node() {
     unset -f node
-    export NVM_DIR=~/.nvm
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
     node "$@"
 }
 
 npm() {
     unset -f npm
-    export NVM_DIR=~/.nvm
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
     source <(npm completion)
     npm "$@"
 }
