7d1463e531db7dc3c15e3d072de5a507da12fb47
Alistair Stead <alistairstead@users.noreply.github.com>
2016-11-03 14:50:15 +0000

    Simplify and create nvm home dir

--- init/50_node.sh
+++ init/50_node.sh
@@ -4,32 +4,9 @@
 # or the command check will fail witin the execution of install_dotfiles
 source $DOTFILES/rc.d/functions.sh
 
-# Confirm if a command exists
-function _nvm_exists () {
-    \command -v ${1} > /dev/null 2>&1 || {
-      return 1;
-    }
-}
+mkdir $HOME/.nvm
 
-function install_nvm() {
-  if ! _nvm_exists 'nvm'; then
-    e_header 'Installing nvm...'
-    curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-    e_header 'Reloading the shell'
-    exec $SHELL -l && echo 'nvm installed!'
-  else
-    e_header 'nvm is already installed'
-  fi
-}
+npm install -g diff-so-fancy tern elm elm-test elm-oracle
 
-function install_node_stable() {
-  nvm install stable
-}
-
-function install_node_global_packages() {
-  npm install -g diff-so-fancy tern elm elm-test elm-oracle
-}
-
-install_nvm && install_node_stable && install_node_global_packages
 
 
