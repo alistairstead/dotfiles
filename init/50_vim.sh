35b1502631e0faba34cb41550f5f643eade68075
Alistair Stead <alistair_stead@me.com>
2016-11-03 21:59:02 +0000

    Add command called without loading Applications

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -2,5 +2,8 @@ mkdir $HOME/.config
 
 ln -s $HOME/.vim $HOME/.config/nvim
 ln -s $HOME/.vimrc $HOME/.config/nvim/init.vim
-    
+
 pip3  install --upgrade neovim
+
+nvim -c ":PlugInstall"
+nvim -c ":UpdateRemotePlugins"
