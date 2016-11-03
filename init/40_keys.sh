13d13bfa7c937bceef5f1fde513d56803e7eed8b
Alistair Stead <alistairstead@users.noreply.github.com>
2016-11-03 12:31:01 +0000

    Create ssh key and copy to github functionality

---
+++ init/40_keys.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+successfully() {
+  $* || (echo "failed" 1>&2 && exit 1)
+}
+
+echo "Checking for SSH key, generating one if it doesn't exist ..."
+  [[ -f ~/.ssh/id_rsa.pub ]] || ssh-keygen -t rsa
+
+echo "Copying public key to clipboard. Paste it into your Github account ..."
+  [[ -f ~/.ssh/id_rsa.pub ]] && cat ~/.ssh/id_rsa.pub | pbcopy
+  successfully open https://github.com/account/ssh
