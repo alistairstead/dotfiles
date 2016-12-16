9cd28431e51ea4746832b1641a1715e74d6dbeff
Alistair Stead <alistair_stead@me.com>
2016-12-16 08:26:48 +0000

    Add the final step to execute the downloaded bin file

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ~
-chsh -s $(which zsh)
 git clone https://github.com/alistairstead/dotfiles
 cd ~/dotfiles
 git submodule update --init --recursive
+./bin/install_dotfiles
