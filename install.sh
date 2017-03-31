e6484487f8ab80cda4e554e4da4a3bbac6c571cf
Alistair Stead <alistair_stead@me.com>
2017-03-31 14:36:32 +0100

    Initialise the zpresto extensions
    Add a call during install to pull the submodules into the `~/.zpresto`
    directory to enable them to be used stright after install

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 cd ~
 git clone https://github.com/alistairstead/dotfiles
+cd ~/.zprezto
+git submodule update --init --recursive
 cd ~/dotfiles
 git submodule update --init --recursive
 ./bin/install_dotfiles
