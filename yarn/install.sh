70f05b24530f9614815b89aec4dbb31bcaba4866
alistairstead <alistair_stead@me.com>
2018-07-27 10:09:22 +0100

    Add prettier and husky

--- yarn/install.sh
+++ yarn/install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-packages=(diff-so-fancy tern nsp snyk elm elm-format elm-live elm-oracle elm-test prettier)
+packages=(diff-so-fancy tern nsp snyk elm elm-format \
+						elm-live elm-oracle elm-test \
+						prettier prettier-plugin-elm husky)
 
 for package in "${packages[@]}"; do
 	if test ! $(which "${package}"); then
