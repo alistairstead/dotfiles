8926faca55c912e1b9bcc8a960faae4a5ce2c34d
Alistair Stead <alistair_stead@me.com>
2019-03-22 12:37:36 +0000

    Remove merge conflict markers

--- yarn/install.sh
+++ yarn/install.sh
@@ -2,7 +2,6 @@ packages=(tern nsp snyk elm elm-format \
 						elm-live elm-oracle elm-test \
 						prettier prettier-plugin-elm husky bash-language-server)
 
-<<<<<<< Updated upstream
 if test $(which yarn); then
 	for package in "${packages[@]}"; do
 		if test ! $(which "${package}"); then
@@ -10,12 +9,3 @@ if test $(which yarn); then
 		fi
 	done
 fi
-=======
-packages=(diff-so-fancy tern nsp snyk elm elm-format elm-live elm-oracle elm-test prettier)
-
-for package in "${packages[@]}"; do
-	if test ! $(which "${package}"); then
-		yarn global add "${package}"
-	fi
-done
->>>>>>> Stashed changes
