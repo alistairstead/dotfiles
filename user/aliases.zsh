6c98ed99c0c224900c9609f9b479ad484a4589ed
Alistair Stead <alistair_stead@me.com>
2019-10-08 14:20:37 +0100

    Add tw alias

--- user/aliases.zsh
+++ user/aliases.zsh
@@ -79,3 +79,5 @@ done
 # Kill all the tabs in Chrome to free up memory
 # [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
 alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
+
+alias tw="fswatch -o {lib,test} | xargs -n1 -I{} mix test --stale"
