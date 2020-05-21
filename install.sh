42c7eca6525ab646ef752d87e856d954b725e362
Alistair Stead <alistair_stead@me.com>
2020-05-21 22:23:45 +0100

    Update install command

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-set -ev
 sudo -v
+# Keep-alive: update existing `sudo` time stamp until install has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 cd ~
 git clone https://github.com/alistairstead/dotfiles .dotfiles
 cd .dotfiles
