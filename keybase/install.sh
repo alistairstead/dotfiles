3bfa259d6839ffd503938d89b2f8869e63f7c849
Alistair Stead <alistair_stead@me.com>
2020-05-21 22:03:46 +0100

    Exclude local use config from CI

--- keybase/install.sh
+++ keybase/install.sh
@@ -26,23 +26,25 @@ else
 	eval "$(gpg-agent --daemon)"
 fi
 
-keybase login
-KEYBASE_KEY_ID=$(keybase pgp list | sed -n -e 's/^.*ID:  //p')
-keybase pgp export -q "${KEYBASE_KEY_ID}" -o ~/keybase.public.key
-keybase pgp export -q "${KEYBASE_KEY_ID}" --secret -o ~/keybase.secret.key
-gpg --import ~/keybase.public.key
-gpg --import ~/keybase.secret.key
 
-# If you need to add your git email to your gpg
-#gpg --edit-key {username}@keybase.io
-# then adduid
+test -z "$CI" &&
+	keybase login
+	KEYBASE_KEY_ID=$(keybase pgp list | sed -n -e 's/^.*ID:  //p')
+	keybase pgp export -q "${KEYBASE_KEY_ID}" -o ~/keybase.public.key
+	keybase pgp export -q "${KEYBASE_KEY_ID}" --secret -o ~/keybase.secret.key
+	gpg --import ~/keybase.public.key
+	gpg --import ~/keybase.secret.key
 
-# Update the trust level to don't show warning messages
-# gpg --edit-key {username}@keybase.io trust quit
+	# If you need to add your git email to your gpg
+	#gpg --edit-key {username}@keybase.io
+	# then adduid
 
-# see https://github.com/pstadler/keybase-gpg-github
+	# Update the trust level to don't show warning messages
+	# gpg --edit-key {username}@keybase.io trust quit
 
-git config --global user.signingkey "$(git config --get user.email)"
-git config --global commit.gpgsign true
-git config --global gpg.program "$(which gpg)"
-rm ~/keybase.public.key ~/keybase.secret.key
+	# see https://github.com/pstadler/keybase-gpg-github
+
+	git config --global user.signingkey "$(git config --get user.email)"
+	git config --global commit.gpgsign true
+	git config --global gpg.program "$(which gpg)"
+	rm ~/keybase.public.key ~/keybase.secret.key
