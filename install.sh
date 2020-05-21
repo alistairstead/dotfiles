922e0b0e2ca4fbbded303a8fcd7d1f902b3b7d33
Alistair Stead <alistair_stead@me.com>
2020-05-21 22:04:28 +0100

    Rename fresh_install to match README docs

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 
 set -ev
-
 sudo -v
-
 cd ~
-
 git clone https://github.com/alistairstead/dotfiles .dotfiles
-
 cd .dotfiles
 
 ./bin/dotfiles symlinks
+./bin/dotfiles install homebrew
 ./bin/dotfiles install
-./bin/dotfiles gitconfig
+test -z "$CI" &&
+	./bin/dotfiles gitconfig
 ./bin/dotfiles antibody
 ./bin/dotfiles brew
 
