6717a743221a5cf5af3a300ba0afa6103730dad1
Alistair Stead <alistair_stead@me.com>
2020-05-22 18:26:00 +0100

    Simplify and document completion config

--- zshrc.d/completion.zsh
+++ zshrc.d/completion.zsh
@@ -1,22 +1,48 @@
 #!/bin/sh
-[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
 
-setopt completealiases         # complete alisases
-setopt extendedglob            # weird & wacky pattern matching - yay zsh!
-setopt nolisttypes             # show types in completion
-setopt autolist                # filename completion
-setopt listpacked              # compact completion lists
-setopt MARK_DIRS               # Append a trailing `/' to all directory names resulting from filename generation (globbing).
-setopt completeinword          # not just at the end
-setopt alwaystoend             # when complete from middle, move cursor
+# the cursor stays where it was and completion is done from both ends.
+setopt COMPLETE_IN_WORD
+# if a completion is performed with the cursor within a word, and a
+# full completion is inserted, the cursor is moved to the end of the
+# word. That is, the cursor is moved to the end of the word if either
+# a single match is inserted or menu completion is performed.
+setopt ALWAYS_TO_END
+# Prevents aliases on the command line from being internally
+# substituted before completion is attempted.
+setopt COMPLETE_ALIASES
+# treat the '#', '~' and '^' characters as part of patterns for
+# filename generation, etc.
+setopt EXTENDED_GLOB
+# do not show the type of each file with a trailing identifying mark.
+setopt NO_LIST_TYPES
+# automatically list choices on an ambiguous completion.
+setopt AUTO_LIST
+# try to make the completion list smaller (occupying less lines) by
+# printing the matches in columns with different widths.
+setopt LIST_PACKED
+# append a trailing '/' to all directory names resulting from
+# filename generation (globbing).
+setopt MARK_DIRS
 
-# Case sensitive, partial word and substring completion
+# case sensitive, partial word and substring completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+# zsh auto rehash commands
+zstyle ':completion:*' rehash true
+# case sensitive, partial word and substring completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+# activate menu selection
+zstyle ':completion:*' menu select=2
+# get zsh to color the completed part of the results when using tab completion.
+zstyle ':completion:*' list-colors "=(#b) #([0-9]#)*=34=36"
 
-# Formatting and messages
+# speed up zsh completion
+zstyle ':completion:*:paths' accept-exact '*(N)'
+zstyle ':completion::complete:*' use-cache on
+zstyle ':completion:*' cache-path ~/.zsh/cache
+
+# completion formatting and messages
 zstyle ':completion:*' verbose yes
 zstyle ':completion:*:descriptions' format $'%{\e[0;33m%} %B%d%b%{\e[0m%}'
 zstyle ':completion:*:messages' format '%d'
@@ -27,24 +53,6 @@ zstyle ':completion:*:manuals' separate-sections true
 zstyle ':completion:*' auto-description 'specify: %d'
 zstyle ':completion:*:default' list-prompt'%S%M matches%s'
 zstyle ':completion:*:prefix:*' add-space true
-
-# Speed up zsh completion
-zstyle ':completion:*:paths' accept-exact '*(N)'
-zstyle ':completion::complete:*' use-cache on
-zstyle ':completion:*' cache-path ~/.zsh/cache
-
-# Zsh auto rehash commands
-zstyle ':completion:*' rehash true
-
-# Case sensitive, partial word and substring completion
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-
-# Activate menu selection
-zstyle ':completion:*' menu select=2
-
-# Get zsh to color the completed part of the results when using tab completion.
-zstyle ':completion:*' list-colors "=(#b) #([0-9]#)*=34=36"
-
 # I really like the kill|all autocomplete functionality of zsh.
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
 zstyle ':completion:*:kill:*' command 'ps -u $(whoami) -o pid,%cpu,tty,cputime,cmd'
