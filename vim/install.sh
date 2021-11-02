a4ab3f9f897a15737a0d425fab4c7b4c7516cb60
alistairstead <alistair_stead@me.com>
2021-11-02 11:04:12 +0000

    feat(vim): add install for Plug

--- vim/install.sh
+++ vim/install.sh
@@ -1,1 +1,4 @@
 #!/bin/sh
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
