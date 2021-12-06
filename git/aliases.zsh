3516d7c0ca38e161f34573738881151b8897139d
alistairstead <alistair_stead@me.com>
2021-12-06 16:02:20 +0000

    feat(git): reduce config and move from hub to gh

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,23 +1,7 @@
 #!/usr/local/env zsh
 
-eval "$(hub alias -s)"
-
-alias gl='git pull --prune'
-alias glg="git log --graph --decorate --oneline --abbrev-commit"
-alias glga="glg --all"
-alias gp='git push origin HEAD'
-alias gpa='git push origin --all'
-alias gd='git diff'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gb='git branch -v'
 alias ga='git add'
-alias gaa='git add -A'
-alias gcm='git commit -m'
-alias gcam='git commit -a -m'
 alias gs='git status -sb'
-alias gcl='git clone'
-alias gcb='git-copy-branch-name'
 
 alias push='git push'
 alias pull='git pull'
