2920703905dfc305eab88290923c9ebfa5a169e0
alistairstead <alistair_stead@me.com>
2021-12-06 16:03:45 +0000

    feat(propmp): move to a quicker prompt

--- zshrc.d/prompt.zsh
+++ zshrc.d/prompt.zsh
@@ -1,10 +1,3 @@
 #!/usr/local/env zsh
 
-# https://github.com/spaceship-prompt/spaceship-prompt/blob/master/docs/options.md
-
-ZSH_AUTOSUGGEST_STRATEGY=(history completion)
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#636d83"
-
-
-# welcome!
-# ponysay -o
+eval "$(starship init zsh)"
