636250f84c91036e8d3738fefdb898a4f98c1612
alistairstead <alistair_stead@me.com>
2022-08-30 17:03:23 +0100

    feat(neovim): switch install to use nightly

--- neovim/install.sh
+++ neovim/install.sh
@@ -2,15 +2,8 @@
 
 if test $(which brew); then
 	brew tap 'neovim/neovim'
-	brew tap 'universal-ctags/universal-ctags'
 
-	brew install neovim || brew upgrade neovim
-	brew install ripgrep || brew upgrade ripgrep
-	brew install --HEAD universal-ctags || brew upgrade universal-ctags
-	brew install the_silver_searcher || brew upgrade the_silver_searcher
-	brew install fd || brew upgrade fd
+	brew install --HEAD neovim || brew reinstall --HEAD neovim
 	brew install fzf || brew upgrade fzf
 fi
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
