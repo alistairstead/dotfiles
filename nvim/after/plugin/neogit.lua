e5018a771812214daad8cc49041835191835c2ae
alistairstead <alistair_stead@me.com>
2023-03-16 11:45:09 +0000

    Update negit config to start in insert mode

--- nvim/after/plugin/neogit.lua
+++ nvim/after/plugin/neogit.lua
@@ -3,7 +3,11 @@ if not ok then
   return
 end
 
-neogit.setup({})
+neogit.setup({
+  disable_commit_confirmation = true,
+  auto_show_console = false,
+  disable_insert_on_commit = false,
+})
 
 local Remap = require('alistairstead.keymap')
 local nnoremap = Remap.nnoremap
