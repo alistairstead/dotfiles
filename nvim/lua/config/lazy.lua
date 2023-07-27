2a68b696cafa6d5c32ad76cc725a80d019e43b4c
alistairstead <alistair_stead@me.com>
2023-07-27 13:59:12 +0100

    wip

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,33 +1,46 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
---Remap space as leader key
-vim.g.mapleader = ' '
-
-require('lazy').setup('plugins', {
-  -- defaults = { lazy = true },
-  install = { colorscheme = { 'tokyonight' } },
-  checker = { enabled = true },
+require('lazy').setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
+    -- import any extras modules here
+    { import = 'lazyvim.plugins.extras.lang.typescript' },
+    { import = 'lazyvim.plugins.extras.lang.json' },
+    -- { import = 'lazyvim.plugins.extras.ui.mini-animate' },
+    -- import/override with your plugins
+    { import = 'lazyvim.plugins.extras.linting.eslint' },
+    { import = 'lazyvim.plugins.extras.formatting.prettier' },
+    { import = 'plugins' },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { 'tokyonight', 'habamax' } },
+  checker = { enabled = true }, -- automatically check for plugin updates
   dev = {
-    path = "~/code/",
+    path = '~/code/',
   },
   performance = {
     rtp = {
+      -- disable some rtp plugins
       disabled_plugins = {
         'gzip',
-        'matchit',
-        'matchparen',
-        'netrwPlugin',
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
         'tarPlugin',
         'tohtml',
         'tutor',
@@ -35,15 +48,4 @@ require('lazy').setup('plugins', {
       },
     },
   },
-  -- debug = true,
 })
-
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'VeryLazy',
-  callback = function()
-    require('config.keymap')
-    require('config.autocmds')
-  end,
-})
-
-vim.keymap.set('n', '<leader>z', '<cmd>:Lazy<cr>', { desc = 'Plugin Manager' })
--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,6 +1,5 @@
 return {
   'goolord/alpha-nvim',
-  event = 'VimEnter',
   opts = function()
     local dashboard = require('alpha.themes.dashboard')
     local logo = [[
@@ -31,29 +30,5 @@ return {
     dashboard.section.footer.opts.hl = 'AlphaFooter'
     dashboard.opts.layout[1].val = 8
     return dashboard
-  end,
-  config = function(_, dashboard)
-    -- close Lazy and re-open when the dashboard is ready
-    if vim.o.filetype == 'lazy' then
-      vim.cmd.close()
-      vim.api.nvim_create_autocmd('User', {
-        pattern = 'AlphaReady',
-        callback = function()
-          require('lazy').show()
-        end,
-      })
-    end
-
-    require('alpha').setup(dashboard.opts)
-
-    vim.api.nvim_create_autocmd('User', {
-      pattern = 'LazyVimStarted',
-      callback = function()
-        local stats = require('lazy').stats()
-        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-        dashboard.section.footer.val = '⚡ Neovim loaded ' .. stats.count .. ' plugins in ' .. ms .. 'ms'
-        pcall(vim.cmd.AlphaRedraw)
-      end,
-    })
   end,
 }
